a2b3a698cc42ec68011c9094662bd70cb925dcad
Willys <willysvarela@gmail.com>
2019-08-15 14:40:04 -0400

    Criando Header - Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,16 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-import "./styles.scss"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1 className="title is-1">Hi people</h1>
-    <a class="button is-outlined is-rounded is-primary">Aqui</a>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div> 
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="hero is-fullheight is-success" >
+  sd
+    </div>
+    
   </Layout>
 )
 

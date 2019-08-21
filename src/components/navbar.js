30f90011e9fd8a5e012e7064c182bb8be993a077
Willys Varela <q_will_p@hotmail.com>
2019-08-20 23:42:12 -0400

    Hero com Slider

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,20 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Navbar() {
+
     const [colorful, setColorful] = useState(true);
+
+  
+
     return (
-        <nav className={(!colorful ? 'navbar is-transparent' : 'navbar ')} role="navigation" aria-label="main navigation" style={{ marginBottom: '-3.25rem'}}>
+        <nav className={(!colorful ? 'navbar is-transparent' : 'navbar ')} role="navigation" aria-label="main navigation" style={{ marginBottom: '-3.25rem' }}>
             <div className="navbar-brand">
-                <a className="navbar-item" href="https://bulma.io"><img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"/></a>
+                <a className="navbar-item" href="https://bulma.io"><img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" /></a>
                 <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbar">
                     <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
-                </a> 
+                    <span aria-hidden="true"></span>
+                </a>
             </div>
             <div id="navbar" className="navbar-menu">
                 <div className="navbar-start">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import bulmaCarousel from 'bulma-carousel/';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="hero is-fullheight is-success" >
-  sd
-    </div>
-    
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <section class="carousel hero is-medium has-carousel">
+        <div id="carousel-demo" class="hero-carousel">
+          <div class="item-1" style={{ backgroundColor: '#000' }}>
+          </div>
+          <div class="item-2" style={{ backgroundColor: '#333' }}>
+          </div>
+          <div class="item-3" style={{ backgroundColor: '#222' }}>
+          </div>
+        </div>
+        <div class="hero-head"></div>
+        <div class="hero-body"></div>
+        <div class="hero-foot"></div>
+      </section>
+    </Layout>
+  )
+}
 
 export default IndexPage
